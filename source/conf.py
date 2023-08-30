4d91d1ddcc434c4099d301cc176f259b71d4a995
xiaoyao <2233865125@qq.com>
2023-08-30 16:58:38 +0800

    update0835

--- source/conf.py
+++ source/conf.py
@@ -40,8 +40,10 @@ html_favicon = "_static/logo.png"
 
 
 extensions = ['recommonmark']
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+
+# from recommonmark.parser import CommonMarkParser
+
+#source_parsers = {
+#    '.md': CommonMarkParser,
+#}
 source_suffix = ['.rst','.md']
